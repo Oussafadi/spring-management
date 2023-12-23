fc9c923ddf2f38ebe6a6a551f9304bd3f42db9a3
Oussafadi <fadi.oussa@outlook.fr>
2023-12-23 23:28:40 +0100

    add dependecies

---
+++ src/main/java/com/example/springbootdepartement/SpringBootDepartementApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdepartement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDepartementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDepartementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootdepartement/SpringBootDepartementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdepartement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDepartementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
